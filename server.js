2ca02fdc8cbcb1c470f356e3495c0936a05699b9
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2022-05-07 23:10:33 +0100

    make it better

--- server.js
+++ server.js
@@ -34,28 +34,6 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-/*
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
-  }
-});
-
-io.on('connection', socket => {
-  socket.emit('me', socket.id);
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('callended');
-  });
-  socket.on('calluser', ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit('calluser', { signal: signalData, from, name });
-  });
-  socket.on('answercall', data => {
-    io.to(data.to).emit('callaccepted', data.signal);
-  });
-});
-
- */
 
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
